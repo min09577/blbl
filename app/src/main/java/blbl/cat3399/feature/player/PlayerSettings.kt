e834f5e341c0b957c70fb5830bf469aa9732794d
min09577 <min09577@github.com>
2026-06-07 16:15:23 +0800

    Release v33.9.0 - Danmaku custom font path

--- app/src/main/java/blbl/cat3399/feature/player/PlayerSettings.kt
+++ app/src/main/java/blbl/cat3399/feature/player/PlayerSettings.kt
@@ -9207,4 +9207,14 @@ internal fun PlayerActivity.showv33PlaybackSkipIntroSecDialog() {
         .show()
 }
 
+
+// 33.9: 弹幕自定义字体路径
+internal fun PlayerActivity.showv33DanmakuCustomFontPathToggle() {
+    val current = BiliClient.prefs.v33DanmakuCustomFontPath
+    AppPopup.Builder(this)
+        .title("弹幕自定义字体路径")
+        .option(switchText(current)) { BiliClient.prefs.v33DanmakuCustomFontPath = !current }
+        .show()
+}
+
 }
