af1e4d3199e7227ef3d8a7c85b3b6ef136804aa0
min09577 <min09577@github.com>
2026-06-07 13:59:23 +0800

    Release v37.10.0 - Reverb preset

--- app/src/main/java/blbl/cat3399/core/prefs/AppPrefs.kt
+++ app/src/main/java/blbl/cat3399/core/prefs/AppPrefs.kt
@@ -2132,6 +2132,11 @@ class AppPrefs(context: Context) {
     var v36VideoAspectFitMode: Int
         get() = prefs.getInt(KEY_V36_VIDEO_ASPECT_FIT_MODE, 0)
         set(value) = prefs.edit().putInt(KEY_V36_VIDEO_ASPECT_FIT_MODE, value).apply()
+    // v37.10: 混响预设
+    var v37AudioReverbPreset: Int
+        get() = prefs.getInt(KEY_V37_AUDIO_REVERB_PRESET, 0)
+        set(value) = prefs.edit().putInt(KEY_V37_AUDIO_REVERB_PRESET, value).apply()
+
     // v37.9: 弹幕等级屏蔽
     var v37DanmakuLevelShield: Int
         get() = prefs.getInt(KEY_V37_DANMAKU_LEVEL_SHIELD, 0)
@@ -3711,3 +3716,4 @@ class AppPrefs(context: Context) {
         private const val KEY_V37_VIDEO_FPS_COUNTER = "v37_video_fps_counter"
         private const val KEY_V37_PLAYBACK_BUFFER_STRATEGY = "v37_playback_buffer_strategy"
         private const val KEY_V37_DANMAKU_LEVEL_SHIELD = "v37_danmaku_level_shield"
+        private const val KEY_V37_AUDIO_REVERB_PRESET = "v37_audio_reverb_preset"
