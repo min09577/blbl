38a60895c5f18a87eed639d7dabb7f7a632ad837
min09577 <min09577@github.com>
2026-06-07 16:34:11 +0800

    Release v36.15.0 - Auto-play next episode

--- app/src/main/java/blbl/cat3399/feature/player/PlayerSettings.kt
+++ app/src/main/java/blbl/cat3399/feature/player/PlayerSettings.kt
@@ -9828,4 +9828,14 @@ internal fun PlayerActivity.showv36SubtitleFontWeightCustomDialog() {
         .show()
 }
 
+
+// 36.15: 自动播放下一集
+internal fun PlayerActivity.showv36PlaylistAutoplayNextEpisodeToggle() {
+    val current = BiliClient.prefs.v36PlaylistAutoplayNextEpisode
+    AppPopup.Builder(this)
+        .title("自动播放下一集")
+        .option(switchText(current)) { BiliClient.prefs.v36PlaylistAutoplayNextEpisode = !current }
+        .show()
+}
+
 }
