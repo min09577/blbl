f364874cf236580263d8474f002541502ab06982
min09577 <min09577@github.com>
2026-06-09 16:53:03 +0800

    Release v720.0.0 - Add v711-v720 via FeaturePrefs711 (150 features)

--- app/src/main/java/blbl/cat3399/core/prefs/FeaturePrefsAccessor.kt
+++ app/src/main/java/blbl/cat3399/core/prefs/FeaturePrefsAccessor.kt
@@ -6,5 +6,7 @@ import blbl.cat3399.BlblApp
  * Lazy accessor for feature preference batches.
  */
 object FeaturePrefsStore {
-    val batch701: FeaturePrefs701 by lazy { FeaturePrefs701(BlblApp.instance) }
+    val batch701: FeaturePrefs701 by lazy { FeaturePrefs701(BlblApp.instance)     val batch711: FeaturePrefs711 by lazy { FeaturePrefs711(BlblApp.instance) }
+}
+    val batch711: FeaturePrefs711 by lazy { FeaturePrefs711(BlblApp.instance) }
 }
