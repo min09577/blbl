7ef310c8f7d22236c396eaea1b2622749a671ede
min <397824888@qq.com>
2026-06-15 01:40:31 +0800

    chore: ktlint import ordering fix for AppToast.kt

--- app/src/main/java/blbl/cat3399/core/ui/AppToast.kt
+++ app/src/main/java/blbl/cat3399/core/ui/AppToast.kt
@@ -3,7 +3,6 @@ package blbl.cat3399.core.ui
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
-import androidx.core.content.ContextCompat
 import android.os.Handler
 import android.os.Looper
 import android.util.TypedValue
@@ -16,6 +15,7 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import blbl.cat3399.R
